d6e71937e48497895292bd8dc1e4711fb9363259
Anushree Anil <anushreeanil@Anushrees-MacBook-Air.local>
2026-06-22 18:49:06 +0530

    changed landing page to sign-in

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://oodqutwsljhvuyotuthu.supabase.co",
-"sb_publishable_nsZvhtzCjz2EePkqPARPMQ_gN5hKa-i"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
